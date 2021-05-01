36856d225a8ab1b200cb6afc30d5bab0eff0409d
Gregor <renekton73@gmail.com>
2021-05-01 21:10:57 +0300

    Complited

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using DI.App.Abstractions;
+using DI.App.Services;
+using DI.App.Services.PL;
+using System.Collections.Generic;
+
+namespace DI.App
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            // Inversion of Control
+            var DbEntity = new Dictionary<int, IDbEntity>();
+            var dbServices = new InMemoryDatabaseService(DbEntity);
+            var userStore = new UserStore(dbServices);
+            var commands = new Dictionary<int, ICommand>();
+            var commandProcessor = new CommandProcessor(userStore, commands);
+            var manager = new CommandManager(commandProcessor);
+            manager.Start();
+        }
+    }
+}
---
+++ Services/PL/Commands/CommandProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DI.App.Abstractions;
+using DI.App.Services.PL.Commands;
+
+namespace DI.App.Services.PL
+{
+    public class CommandProcessor : ICommandProcessor
+    {
+        private readonly Dictionary<int, ICommand> commands;
+        public CommandProcessor(UserStore userStore, Dictionary<int,ICommand>commands)
+        {
+            
+            this.commands = commands;
+            var addUsers = new AddUserCommand(userStore);
+            var listUsers = new ListUsersCommand(userStore);
+
+            this.commands.Add(addUsers.Number, addUsers);
+            this.commands.Add(listUsers.Number, listUsers);
+        }
+
+        public void Process(int number)
+        {
+            if (!this.commands.TryGetValue(number, out var command)) return;
+
+            command.Execute();
+        }
+
+        public IEnumerable<ICommand> Commands => this.commands.Values.AsEnumerable();
+    }
+}
