3a1022e1cd73f7ef92440be0ed8524dba5ae7ce7
danil-polikanov <83316005+danil-polikanov@users.noreply.github.com>
2021-07-12 07:17:01 +0300

    Add files via upload

---
+++ Bulls and Cows/Bulls and Cows/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bulls_and_Cows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logic logic = new Logic();
+            logic.Game();
+        }
+    }
+}
+
+
+
